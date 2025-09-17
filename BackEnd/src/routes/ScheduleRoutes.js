cc5dead8e8452c40549ee8468a2bcdd666041d24
llOrTeGall <IvanOrtega_97@hotmail.com>
2025-09-16 19:14:27 -0500

    TEST 1

--- BackEnd/src/routes/ScheduleRoutes.js
+++
@@ -1,9 +0,0 @@
-import {Router} from 'express';
-import {changeSchedule, detectSchedule} from '../Controllers/ScheduleController.js';
-
-const router = Router();
-
-router.get('/schedule', detectSchedule);
-router.post('/change-schedule', changeSchedule);
-
-export {router};
--- FrontEnd/src/App.tsx
+++ FrontEnd/src/App.tsx
@@ -1,11 +1,8 @@
-import { Card } from "./components/Card";
-import './App.css'
-
 function App() {
 
   return (
     <div className="bg-black w-full h-screen flex items-center justify-center">
-      <Card />
+      
     </div>
   )
 }
